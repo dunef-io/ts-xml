6a7e9e724bcb6f5d44db79c1d0cc03e1dcc2d76e
Nick <nick@dunef.io>
2025-07-26 00:04:53 +0200

    fix

--- src/xml/utils/assertions.ts
+++ src/xml/utils/assertions.ts
@@ -1,7 +1,7 @@
-import { XmlDocumentImpl } from '../internal/document';
+import { XmlDocument } from '../document';
 
 export function assertDocumentParseInvariants(input: string): void {
-    const document = XmlDocumentImpl.parse(input);
+    const document = XmlDocument.parse(input);
     expect(document.toXmlString()).toEqual(input);
     const copy = document.copy();
     expect(document.toXmlString()).toEqual(copy.toXmlString());
