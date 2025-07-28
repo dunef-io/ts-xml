16c004ab6912950f550090f954823ab59d0ff987
Nick <nick@dunef.io>
2025-07-28 14:39:56 +0200

    remove unneeded files

--- src/xml/data.ts
+++
@@ -1,9 +0,0 @@
-import { XmlDataInterface, XmlNodeInterface } from './interfaces';
-import { XmlNode } from './node';
-import { XmlVisitorInterface } from './interfaces/visitor';
-
-export abstract class XmlData implements XmlDataInterface {
-    constructor(public value: string) { };
-
-    abstract accept(visitor: XmlVisitorInterface): void;
-}
